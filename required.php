a990c7a0a752f4131591e29cec22fb6cd7b248e5
Mikhail Sergeev <msergeev06@gmail.com>
2016-10-14 15:48:20 +0300

    Setup required package dates

---
+++ required.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * MSergeev
+ * @package calendar
+ * @author Mikhail Sergeev
+ * @copyright 2016 Mikhail Sergeev
+ */
+
+//Обязательные пакеты, без которых пакет работать не будет
+$arRequiredPackages = array(
+	"dates"
+);
+//Дополнительные пакеты, которые расширят функционал, но и без них пакет будет работать
+$arAdditionalPackages = array();
